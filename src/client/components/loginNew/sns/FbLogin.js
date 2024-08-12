5d731e15f51b30097bacc88c33e01f3950b4be5a
andriantsoy <andriantsoy@gmail.com>
2024-08-12 19:53:12 +0900

    migration work

--- src/client/components/loginNew/sns/FbLogin.js
+++ src/client/components/loginNew/sns/FbLogin.js
@@ -1,13 +1,14 @@
-import React, { useContext, useEffect, useState } from 'react';
-import axios from 'axios';
-import { useHistory } from 'react-router-dom';
-import { Box, Grid } from '@material-ui/core';
-import FacebookIcon from '../../../img/facebook-logo.png';
-import AuthContext from '../../../context/AuthContext';
-import SocialButtonNew from './SocialButtonNew';
-import InstagramDialog from '../../login/InstagramDialog';
-import InstagramSelectDialog from '../../login/InstagramSelectDialog';
-import StyledButton from '../../../containers/StyledButton';
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+import { Box, Grid } from "@material-ui/core";
+import FacebookIcon from "../../../img/facebook-logo.png";
+import AuthContext from "../../../context/AuthContext";
+import SocialButtonNew from "./SocialButtonNew";
+import InstagramDialog from "../../login/InstagramDialog";
+import InstagramSelectDialog from "../../login/InstagramSelectDialog";
+import StyledButton from "../../../containers/StyledButton";
+import { fbGetCredentials } from "../../../lib/fb";
 
 function FbLogin() {
   const history = useHistory();
@@ -27,65 +28,78 @@ function FbLogin() {
   }
 
   function saveLoginInfo(info) {
-    localStorage.setItem('loginInfo', JSON.stringify({
-      ...info
-    }));
-    history.push('/Join');
+    localStorage.setItem(
+      "loginInfo",
+      JSON.stringify({
+        ...info,
+      })
+    );
+    history.push("/Join");
   }
 
   function addInstagram(selectedId) {
     window.FB.getLoginStatus((response) => {
-      if (response.status === 'connected') {
+      if (response.status === "connected") {
         const { accessToken, userID } = response.authResponse;
-        const selectedInfo = instaAccounts.filter(item => item.id === selectedId);
+        const selectedInfo = instaAccounts.filter((item) => item.id === selectedId);
         const loginInfo = {
-          type: 'facebook', accessToken, fbId: userID, instagramInfo: selectedInfo[0]
+          type: "facebook",
+          accessToken,
+          fbId: userID,
+          instagramInfo: selectedInfo[0],
         };
         saveLoginInfo(loginInfo);
       } else {
-        alert('The user isn\'t logged in to Facebook');
+        alert("The user isn't logged in to Facebook");
       }
     });
   }
 
-  function facebookLogin() {
-    FB.login((loginRes) => {
-      if (loginRes.authResponse) {
-        const { accessToken, userID } = loginRes.authResponse;
+  async function facebookLogin() {
+    try {
+      const { accessToken, userID } = await fbGetCredentials();
 
-        axios.post('/api/TB_INFLUENCER/facebookLoginNew', {
-          facebookToken: accessToken,
-          facebookUserId: userID
-        }).then((res) => {
-          if (res.status === 200) {
-            const {
-              social_type, userToken, userName, userPhoto
-            } = res.data;
-            auth.login(userToken, userName, social_type, userPhoto);
-            history.push('/');
-          } else if (res.status === 202) {
-            setInstaAccounts(res.data.data);
-            selectAccountDialog();
-          } else if (res.status === 201) {
-            const instagramInfo = res.data.data;
-            const loginInfo = {
-              type: 'facebook', accessToken, fbId: userID, instagramInfo
-            };
-            saveLoginInfo(loginInfo);
-          }
-        }).catch((err) => {
-          alert(err.response.data.message);
-        });
-      } else {
-        console.log('not connected');
+      const res = await axios.post("/api/TB_INFLUENCER/facebookLoginNew", {
+        facebookToken: accessToken,
+        facebookUserId: userID,
+      });
+
+      if (res.status === 200) {
+        const { social_type, userToken, userName, userPhoto } = res.data;
+        auth.login(userToken, userName, social_type, userPhoto);
+        history.push("/");
+      } else if (res.status === 202) {
+        setInstaAccounts(res.data.data);
+        selectAccountDialog();
+      } else if (res.status === 201) {
+        const instagramInfo = res.data.data;
+        const loginInfo = {
+          type: "facebook",
+          accessToken,
+          fbId: userID,
+          instagramInfo,
+        };
+        saveLoginInfo(loginInfo);
       }
-    }, { scope: 'public_profile, email, pages_show_list, instagram_basic, instagram_manage_insights' });
+    } catch (e) {
+      alert(e.message);
+    }
   }
 
   return (
     <React.Fragment>
-      <SocialButtonNew onClick={toggleInstaDialog} icon={FacebookIcon} text="페이스북 로그인" background="#3B5998" color="#FFFFFF" />
-      <InstagramDialog open={instaDialogOpen} closeDialog={toggleInstaDialog} facebookLogin={facebookLogin} />
+      <SocialButtonNew
+        onClick={toggleInstaDialog}
+        icon={FacebookIcon}
+        text="페이스북 로그인"
+        background="#3B5998"
+        color="#FFFFFF"
+      />
+      <InstagramDialog
+        open={instaDialogOpen}
+        closeDialog={toggleInstaDialog}
+        facebookLogin={facebookLogin}
+      />
       <InstagramSelectDialog
         open={instaSelectDialogOpen}
         closeDialog={selectAccountDialog}
